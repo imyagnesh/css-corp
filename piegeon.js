ae1b0969ac561929499f0f0a8c0ff432ee0d1c34
Abinayaa <Abinayaa.Kalyanasundaram@ind.csscorp.com>
2021-12-05 19:17:10 +0530

    Removed JS related files

--- piegeon.js
+++
@@ -1,9 +0,0 @@
-( function () {
-    var bird = 'Pidgeons';
-    if ( typeof bird === 'undefined' ){
-        var bird = 'Rubber Duck';
-        console.log('Ernie loves his ' + bird );
-    } else {
-        console.log('Bert loves his ' + bird );
-    }
-}() );
