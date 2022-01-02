b14306c97e6650cf758dff1198adcd0af522c48b
Annie <annie@gmail.com>
2022-01-02 21:54:11 +0530

    ReactTask: Code optimization - Minimal Representation Of UI State

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,19 @@ import Input from './Input';
 import SetUnit from './SetUnits';
 import SearchResults from './SearchResults';
 import WeatherReport from './WeatherReport';
-import Regenerator from 'regenerator-runtime/runtime';
-import { OPENWEATHER_API_KEY, DEFAULT_CITY_WEATHER, NO_WEATHER_REPORT_ERROR } from '../common/constants';
+import { OPENWEATHER_API_KEY, DEFAULT_CITY_WEATHER, OPENWEATHER_CELCIUS_VALUE } from '../common/constants';
 import _, { debounce } from 'lodash';
+
+var temperature, minTemp, maxTemp, windSpeed, windDirection, pressure, feelsLike, country, humidity, description, icon;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      temperature: undefined,
-      minTemp: undefined,
-      maxTemp: undefined,
-      windSpeed: undefined,
-      windDirection: undefined,
-      pressure: undefined,
-      feelsLike: undefined,
       city: DEFAULT_CITY_WEATHER,
-      country: undefined,
-      humidity: undefined,
-      description: undefined,
-      units: undefined,
-      icon: undefined,
-      cities: [],
-      error: undefined
+      fetchedCities: [],
+      units: OPENWEATHER_CELCIUS_VALUE,
+      hasErrorInWeatherFetch: false
     }
   }
 
@@ -35,17 +25,17 @@ class App extends Component {
 
   inputRef = React.createRef();
   selectRef = React.createRef();
-  
+
   searchLocations = (e) => {
       e.preventDefault();
       const cityKeyword = this.inputRef.current.value;
 
-      fetch(`http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=5&offset=0&namePrefix=${cityKeyword}&minPopulation=1000039`)
+      fetch(`http://geodb-free-service.wirefreethought.com/v1/geo/cities?limit=5&offset=0&namePrefix=${cityKeyword}&minPopulation=1000039&types=city`)
       .then((res) => res.json())
       .then((json) => { 
         console.log(json);
         this.setState ({
-          cities: [...json.data]
+          fetchedCities: [...json.data]
         })
       })
       .catch(err => {
@@ -58,7 +48,6 @@ class App extends Component {
   }
 
   getWeather = async (e)  =>  {
-    //var city = e != null ? e.target.innerText : this.state.city;
     var city = this.getCity(e);
     const units = this.selectRef.current.value;
     var dataWeather = null;
@@ -68,39 +57,29 @@ class App extends Component {
                     dataWeather = json;
                     console.log(dataWeather);
                     if (dataWeather != null) {
+                      temperature = dataWeather.main.temp;
+                      minTemp = dataWeather.main.temp_min;
+                      maxTemp = dataWeather.main.temp_max;
+                      windSpeed = dataWeather.wind.speed;
+                      windDirection = dataWeather.wind.deg;
+                      pressure = dataWeather.main.pressure;
+                      feelsLike = dataWeather.main.feels_like;
+                      country = dataWeather.sys.country;
+                      humidity = dataWeather.main.humidity;
+                      description = dataWeather.weather[0].description;
+                      icon = "https://openweathermap.org/img/wn/"+dataWeather.weather[0].icon+"@2x.png";
+                      
                       this.setState({
-                        temperature: dataWeather.main.temp,
-                        minTemp: dataWeather.main.temp_min,
-                        maxTemp: dataWeather.main.temp_max,
-                        windSpeed: dataWeather.wind.speed,
-                        windDirection: dataWeather.wind.deg,
-                        pressure: dataWeather.main.pressure,
-                        feelsLike: dataWeather.main.feels_like,
                         city: dataWeather.name,
-                        country: dataWeather.sys.country,
-                        humidity: dataWeather.main.humidity,
-                        description: dataWeather.weather[0].description,
                         units: this.selectRef.current.value,
-                        icon: "https://openweathermap.org/img/wn/"+dataWeather.weather[0].icon+"@2x.png",
-                        error: ""
+                        hasErrorInWeatherFetch: false
                       });
                     }
                   })
                   .catch(err => {
                     this.setState({
-                      temperature: undefined,
-                      minTemp: undefined,
-                      maxTemp: undefined,
-                      windSpeed: undefined,
-                      windDirection: undefined,
-                      pressure: undefined,
-                      feelsLike: undefined,
-                      city: undefined,
-                      country: undefined,
-                      humidity: undefined,
-                      description: undefined,
-                      units: undefined,
-                      error: NO_WEATHER_REPORT_ERROR
+                        city: dataWeather.name,
+                        hasErrorInWeatherFetch: true
                     });
                   });
   }
@@ -108,7 +87,6 @@ class App extends Component {
   searchLocations = debounce(this.searchLocations, 500);
 
   render() {
-    const { temperature, minTemp, maxTemp, windSpeed, windDirection, pressure, feelsLike, city, country, humidity, description, icon, units, error } = this.state;
     return (
       <div className="w-7/12 m-auto container p-2 bg-slate-100">
         <div className="border-solid border-b-2 border-red-800 text-2xl tracking-wide font-bold site-heading my-3">
@@ -119,7 +97,7 @@ class App extends Component {
             <Input ref={this.inputRef} searchLocations={this.searchLocations} />
             <SetUnit ref={this.selectRef} getWeather={this.getWeather} />
           </div>
-          <SearchResults cities={this.state.cities} getWeather={this.getWeather} />
+          <SearchResults cities={this.state.fetchedCities} getWeather={this.getWeather} />
           <WeatherReport 
             temperature={temperature}
             minTemp={minTemp}
@@ -128,12 +106,12 @@ class App extends Component {
             windDirection={windDirection}
             pressure={pressure}
             feelsLike={feelsLike}
-            city={city}
+            city={this.state.city}
             country={country}
             humidity={humidity}
             description={description}
-            error={error}
-            units={units}
+            hasErrorInWeatherFetch = {this.state.hasErrorInWeatherFetch}
+            units={this.state.units}
             icon={icon}
           />
         </div>
--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,71 +1,62 @@
 import React, { memo } from "react";
 import PropTypes from 'prop-types';
-import { OPENWEATHER_CELCIUS_VALUE } from '../common/constants';
+import { OPENWEATHER_CELCIUS_VALUE, NO_WEATHER_REPORT_ERROR } from '../common/constants';
 
 const WeatherReport = (props) => {
-	const { temperature, minTemp, maxTemp, windSpeed, windDirection, pressure, feelsLike, city, country, humidity, description, icon, units, error } = props;
-	return (
-		<div className="bg-white p-1">
-		{	
-			city && country && 
-			<div className="grid grid-cols-3">
-				<div className="my-3 p-2 col-span-2">	
-					<h3 className="text-2xl font-bold tracking-wide">{ city }</h3>
-					<div className="uppercase text-slate-500 font-bold text-xs">
-						<span>{ description }</span> | Feels like <span>{ feelsLike } &deg;{units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</span>
+	const { temperature, minTemp, maxTemp, windSpeed, windDirection, pressure, feelsLike, city, humidity, description, icon, units, hasErrorInWeatherFetch } = props;
+	if (hasErrorInWeatherFetch) {
+		return <p className="text-red-600 font-bold">{ NO_WEATHER_REPORT_ERROR }</p>;
+	}
+	else {
+		return (
+			<div className="bg-white p-1">
+			{	
+				city &&
+				<div className="grid grid-cols-3">
+					<div className="my-3 p-2 col-span-2">	
+						<h3 className="text-2xl font-bold tracking-wide">{ city }</h3>
+						<div className="uppercase text-slate-500 font-bold text-xs">
+							<span>{ description }</span> | Feels like <span>{ feelsLike } &deg;{units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</span>
+						</div>
+					</div>
+					<div>
+						<img src={icon} />
 					</div>
 				</div>
+			}
+			{ 	
+				temperature && 
 				<div>
-					<img src={icon} />
+					<div className="grid grid-cols-3 my-1 space-y-1.5 space-x-9">
+						<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-red-500 to-pink-500"> Current Temperature <br/> {temperature} &deg;{units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</div>
+						<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-red-500 to-pink-500"> Maximum Temperature <br/> { maxTemp} &deg;{ units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</div>
+						<div className="h-24 text-white p-2 uppercase text-center rounded bg-gradient-to-r from-red-500 to-pink-500"> Minimum Temperature <br/> { minTemp} &deg;{ units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</div>
+					</div>
 				</div>
+			}
+			{ 	
+				<div> 
+					<div className="grid grid-cols-3 my-1 space-y-1.5 space-x-9">
+						<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-indigo-500 to-pink-500"> Wind Speed <br/> { windSpeed} meter/sec</div>
+						<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-indigo-500 to-pink-500"> Wind Direction <br/> { windDirection} degrees</div>
+					</div></div>
+			}
+			{ 	
+				pressure && <div>
+					<div className="grid grid-cols-3 my-1 space-y-1.5 space-x-9">
+						<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-green-400 to-blue-500"> Pressure <br/> { pressure} hpa</div>
+						<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-green-400 to-blue-500"> Humidity <br/> { humidity}%</div>
+					</div></div>
+			}
 			</div>
-		}
-		{ 	
-			temperature && 
-			<div>
-				<div className="grid grid-cols-3 my-1 space-y-1.5 space-x-9">
-					<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-red-500 to-pink-500"> Current Temperature <br/> {temperature} &deg;{units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</div>
-					<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-red-500 to-pink-500"> Maximum Temperature <br/> { maxTemp} &deg;{ units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</div>
-					<div className="h-24 text-white p-2 uppercase text-center rounded bg-gradient-to-r from-red-500 to-pink-500"> Minimum Temperature <br/> { minTemp} &deg;{ units == OPENWEATHER_CELCIUS_VALUE ? "C" : "F"}</div>
-				</div>
-			</div>
-		}
-		{ 	
-			 windSpeed && <div> 
-				<div className="grid grid-cols-3 my-1 space-y-1.5 space-x-9">
-					<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-indigo-500 to-pink-500"> Wind Speed <br/> { windSpeed} meter/sec</div>
-					<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-indigo-500 to-pink-500"> Wind Direction <br/> { windDirection} degrees</div>
-				</div></div>
-		}
-		{ 	
-			 pressure && <div>
-				<div className="grid grid-cols-3 my-1 space-y-1.5 space-x-9">
-					<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-green-400 to-blue-500"> Pressure <br/> { pressure} hpa</div>
-					<div className="h-24 text-white uppercase p-2 text-center rounded bg-gradient-to-r from-green-400 to-blue-500"> Humidity <br/> { humidity}%</div>
-				</div></div>
-		}
-		{ 
-			 error && <p className="text-red-600 font-bold">{  error }</p>  
-		}
-		</div>
-	);
+		);
+	}
 };
 
 WeatherReport.propTypes = {
-	temperature: PropTypes.number,
-	minTemp: PropTypes.number,
-	maxTemp: PropTypes.number,
-	windSpeed: PropTypes.number,
-	windDirection: PropTypes.number,
-	pressure: PropTypes.number,
-	feelsLike: PropTypes.number,
 	city: PropTypes.string,
-	country: PropTypes.string,
-	humidity: PropTypes.number,
-	description: PropTypes.string,
 	units: PropTypes.string,
-	icon: PropTypes.string,
-	error: PropTypes.string,
+	hasErrorInWeatherFetch: PropTypes.bool.isRequired,
 };
 
 export default memo(WeatherReport);
